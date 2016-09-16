0d0a0b85449a0837ceca9818f5c23c0e9413a77e
Giorgos Chantzialexiou <georgeha98@gmail.com>
2016-09-16 16:26:34 -0400

    Parralel Formation of Connected Components Update

--- leaflet_finder/Spark/Spark-win-networkx/leafletfinder.py
+++ leaflet_finder/Spark/Spark-win-networkx/leafletfinder.py
@@ -4,21 +4,62 @@ import networkx as nx
 from pyspark import SparkContext
 from time import time
 
+"""
+## Algorithm
+credit to https://www.cs.rice.edu/~vs3/comp422/lecture-notes/comp422-lec24-s08-v2.pdf  page 38
 
 
-def find_edges((window,index),cutoff=15.00):
+- partition the adjacency matrix across processors
+- run indepentent connected components on each processor (find_partial_connected_components)
+- merge spanning forests until one remains  (unify_components)
+
+For Merging:
+I use intersection and union functions:
+- intersection: I check if there is at least one common node, which means MSTs are connected
+- union: if there is a common node, I do union and add to the set of connected componnents, otherwise I add each node alone.
+
+I run the merging function pairwise, and repeat till only one list remain.
+
+"""
+
+def find_partial_connected_components(data,cutoff=15.00):
     
-    frame_list = np.zeros((len(window[0]),len(window[1])), dtype=bool)
+    window = data[0]
+    i_index = data[1][0]
+    j_index = data[1][1]
+    
+    import networkx as nx
     graph = nx.Graph()
     
     for i in range(0,len(window[0])):
         for j in range(0,len(window[1])):
             if np.sqrt(sum((window[0][i] - window[1][j]) ** 2)) <= cutoff:
-            	graph.add_edge(i+index,j+index)    # fix indexes
+                graph.add_edge(i+i_index,j+j_index)    # fix indexes
 
- 	connected_components = nx.connected_components(graph)
+    connected_components = nx.connected_components(graph)
+    cc = []
+    for x in connected_components:
+        cc.append(x)
 
-    return connected_components
+    return  cc
+
+
+def unify_components(cc1,cc2):
+    unified_componnents = []
+    for x in cc1:
+        for y in cc2:
+            if len(x.intersection(y))!=0:
+                union = x.union(y)
+                if not union in unified_componnents:
+                    unified_componnents.append(x.union(y))
+            else:
+                if not x in unified_componnents:
+                    unified_componnents.append(x)
+                if not y in unified_componnents:
+                    unified_componnents.append(y)
+
+    return unified_componnents
+
 
 if __name__=="__main__":
 
@@ -34,10 +75,10 @@ if __name__=="__main__":
     sc = SparkContext(appName="PythonLeafletFinder")    
     
     
-    #coord_matrix = np.load(filename)
-    coord_matrix = np.random.randint(0,2,(100,100))    ## command for testing
-    #coord_matrix_broadcast = sc.broadcast(coord_matrix)
-    arraged_coord = list()
+    coord_matrix = np.load(filename)
+    matrix_size = coord_matrix.shape[0]  # Ask yiannis if this is the matrix size, cause it was accidentaly deleted
+
+    arranged_coord = list()
     for i in range(1,matrix_size+1,part_size):
         for j in range(i,matrix_size,part_size):
             # The arranged_elem contains a tuple with the data needed to calculate
@@ -47,12 +88,6 @@ if __name__=="__main__":
             arranged_elem = ([coord_matrix[i-1:i-1+part_size],coord_matrix[j-1:j-1+part_size]],[i,j])
             arranged_coord.append(arranged_elem)
 
-    partition_matrix = np.zeros((matrix_size/part_size,matrix_size/part_size))
-    step = 0
-    for i in range(0,matrix_size/part_size):
-        for j in range(i,matrix_size/part_size):
-            partition_matrix[i,j] =  step + (j - i)
-        step = step + (matrix_size/part_size) - i
 
     print len(arranged_coord)
     dist_Matrix = sc.parallelize(arranged_coord,len(arranged_coord))
@@ -64,24 +99,16 @@ if __name__=="__main__":
     # >>> sc.parallelize(["a", "b", "c", "d"], 3).zipWithIndex().collect()
     # [('a', 0), ('b', 1), ('c', 2), ('d', 3)]
 
-    dist_Matrix = dist_Matrix.zipWithIndex()  #key-value pairs
 
-    edge_list = dist_Matrix.map(find_edges)
+    connected_components = dist_Matrix.map(find_partial_connected_components).reduce(unify_components) 
 
-
-
-    adj_matrix = np.zeros((matrix_size,matrix_size),dtype=bool)
-    for element in edge_list.collect():
-        pos = np.where(partition_matrix == element[1])
-        #print element[1],element[0].shape, pos
-        adj_matrix[pos[0][0]*part_size:((pos[0][0]+1)*part_size),pos[1][0]*part_size:((pos[1][0]+1)*part_size)] = element[0]
     
-    time_to_create_adj_matrix = time()
-
 
     indices = [np.sort(list(g)) for g in subgraphs]
     np.save('components.npz.npy',indices)
 
-    print 'Total time to create the Adjacency Matrix: %i sec'  % (time_to_create_adj_matrix - start_time)
-    print 'Time to calculate the Connected Components: %i sec ' % (time() - time_to_create_adj_matrix)
+    stop_time = time()
+
+
+    print 'Total Time of execution is : %i sec ' % (stop_time - start_time)
     

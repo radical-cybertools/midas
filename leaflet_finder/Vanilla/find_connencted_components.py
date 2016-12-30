b692ccede9217e350d9ebca0314d641cc2ba56c9
iparask <ioannis.paraskevakos@rutgers.edu>
2016-12-30 17:11:58 -0500

    Added help message in scripts

--- leaflet_finder/Vanilla/find_connencted_components.py
+++ leaflet_finder/Vanilla/find_connencted_components.py
@@ -6,27 +6,37 @@ __license__   = "MIT"
 import numpy as np
 import sys
 import networkx as nx
-import matplotlib.pyplot as pplt
+import argparse
+from time import time
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("atomNum", help="Total Number of atoms in the system")
+    parser.add_argument("win_size", help="Dimension size of the submatrix")
+    args = parser.parse_args()
 
-
-
-    args = sys.argv[1:]
-    NUMBER_OF_TRAJECTORIES = int(sys.argv[1])
-    WINDOW_SIZE = int(sys.argv[2])
-  
+    NUMBER_OF_ATOMS = int(args.atomNum)
+    WINDOW_SIZE = int(args.win_size)
+    
+    start = time()
+    
     #Adjacency matrix 
-    adj_matrix = np.empty([NUMBER_OF_TRAJECTORIES,NUMBER_OF_TRAJECTORIES],dtype='bool')
+    adj_matrix = np.empty([NUMBER_OF_ATOMS,NUMBER_OF_ATOMS],dtype='bool')
 
-    for i in range(1,NUMBER_OF_TRAJECTORIES+1,WINDOW_SIZE):
-        for j in range(i,NUMBER_OF_TRAJECTORIES,WINDOW_SIZE):
+    for i in range(1,NUMBER_OF_ATOMS+1,WINDOW_SIZE):
+        for j in range(i,NUMBER_OF_ATOMS,WINDOW_SIZE):
             data = np.load("distances_%d_%d.npz.npy" % (i-1,j-1))
             adj_matrix[i-1:i-1+WINDOW_SIZE,j-1:j-1+WINDOW_SIZE]=data
+    
+    read_time = time()
 
     graph = nx.Graph(adj_matrix)
-    nx.write_adjlist(graph, 'graph.adjlist')
+    #nx.write_adjlist(graph, 'graph.adjlist')
     subgraphs = nx.connected_components(graph)
+    exec_time = time()
     indices = [np.sort(list(g)) for g in subgraphs]
     np.save('components.npz.npy',indices)
+    write_time = time()
+
+    print "%f,%f,%f"%(read_time - start, exec_time - read_time, write_time - exec_time)
--- leaflet_finder/Vanilla/leaflet_finder.py
+++ leaflet_finder/Vanilla/leaflet_finder.py
@@ -5,9 +5,12 @@ __license__   = "MIT"
 
 import os
 import sys
+import argparse
+
+os.environ['RADICAL_PILOT_VERBOSE'] = 'DEBUG'
+
 import radical.pilot as rp
 import copy
-import networkx as nx
 import MDAnalysis as mda
 import numpy as np
 from datetime import datetime
@@ -15,7 +18,6 @@ from datetime import datetime
 
 #SHARED_INPUT_FILE = 'input.txt'
 MY_STAGING_AREA = 'staging:///'
-os.environ['RADICAL_PILOT_DBURL'] = "mongodb://sean:1234@ds019678.mlab.com:19678/pilot_test"
 
 # READ: The RADICAL-Pilot documentation: 
 #   http://radicalpilot.readthedocs.org/en/latest
@@ -26,99 +28,69 @@ os.environ['RADICAL_PILOT_DBURL'] = "mongodb://sean:1234@ds019678.mlab.com:19678
 
 #------------------------------------------------------------------------------
 #
-def pilot_state_cb (pilot, state):
-
-    if not pilot:
-        return
-
-    print "[Callback]: ComputePilot '%s' state: %s." % (pilot.uid, state)
-
-    if state == rp.FAILED:
-        sys.exit (1)
-
-#------------------------------------------------------------------------------
-#
-def unit_state_cb (unit, state):
-
-    if not unit:
-        return  
-
-    global CNT
-
-    print "[Callback]: unit %s on %s: %s." % (unit.uid, unit.pilot_id, state)
-
-    if state == rp.FAILED:
-        print "stderr: %s" % unit.stderr
-        sys.exit(2)
-#------------------------------------------------------------------------------
-#
 if __name__ == "__main__":
 
-        # Read the number of the divisions you want to create
-    args = sys.argv[1:]
-    if len(args) < 3:
-        print "Usage: "
-        print "python leaflet_finder.py  <window size> #cores <uni filename> <traj filename> <report file name>"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("win_size", help="Dimension size of the submatrix")
+    parser.add_argument("cores", help="Number of cores that will be requested")
+    #parser.add_argument("uni", help="Universe File, extension tpr")
+    #parser.add_argument("trajs", help="Trajectory File, extension xtc")
+    parser.add_argument("atomFile",help="The numpy file that contains the positions of the atoms")
+    parser.add_argument("atomNum",help="The totatl number of atoms in the atomFile")
+    parser.add_argument("session", help="The RADICAL-Pilot Session name")
+    args = parser.parse_args()
+
+    win_size = int(args.win_size)
+    CPUs = int(args.cores) # number of cores
+    #uni_filename = args.uni
+    #traj_filename = args.trajs
+    atom_file = args.atomFile
+    session_name = args.session
+    '''
+    try:
+        universe=mda.Universe(uni_filename, traj_filename)
+    except IOError:
+        print "Missing data-set files! Check the name of the dataset"
         sys.exit(-1)
-    WINDOW_SIZE = int(sys.argv[1])
-    CPUs = int(sys.argv[2]) # number of cores
-    coord_numpy_file=sys.argv[3]
-    #uni_filename=sys.argv[3]
-    #traj_filename = sys.argv[4]
-    #report_name = sys.argv[5]
 
-    #try:
-    #    universe=mda.Universe(uni_filename, traj_filename)
-    #except IOError:
-    #    print "Missing data-set files! Check the name of the dataset"
-    #    sys.exit(-1)
-
-    #selection = universe.select_atoms('name P*')
-    coord = np.load(coord_numpy_file)
-    data=open('input.txt','w')
-    #for i in range(0,100):
-    for coordinates in coord:
-        data.write('%s,%s,%s\n'%(coordinates.tolist()[0],coordinates.tolist()[1],coordinates.tolist()[2]))
-        #data.write('%s,%s,%s\n'%(coord[i].tolist()[0],coord[i].tolist()[1],coord[i].tolist()[2]))
-
-    data.close()
-
-    total_file_lines = coord.shape[0]
+    selection = universe.select_atoms('name P*')
+    np.save('trajectories.npy',selection.positions)
+    
+    total_file_lines = selection.positions.shape[0]
+    '''
+    total_file_lines = args.atomNum
     cutoff=15.0
 
     try:
         # Create a new session.
-        session = rp.Session()
-        print "session id: %s" % session.uid
-        c = rp.Context('ssh')
-        c.user_id = "solejar"
-        session.add_context(c)
+        session = rp.Session(name=session_name)
+        #print "session id: %s" % session.uid
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
-        print "Initializing Pilot Manager ..."
+        #print "Initializing Pilot Manager ..."
         pmgr = rp.PilotManager(session=session)
 
-        pmgr.register_callback(pilot_state_cb)
+        #pmgr.register_callback(pilot_state_cb)
 
         # ----- CHANGE THIS -- CHANGE THIS -- CHANGE THIS -- CHANGE THIS ------
         # 
         pdesc = rp.ComputePilotDescription ()
         pdesc.resource = "xsede.comet" # NOTE: This is a "label", not a hostname
-        pdesc.runtime  = 80 # minutes
+        pdesc.runtime  = 120 # minutes
         pdesc.cores    = CPUs
         pdesc.cleanup  = False
-        #pdesc.queue    = 'development'
         pdesc.project  = 'unc100'
 
         # submit the pilot.
-        print "Submitting Compute Pilot to Pilot Manager ..."
+        #print "Submitting Compute Pilot to Pilot Manager ..."
         pilot = pmgr.submit_pilots(pdesc)
 
         cu_full_list=list()
 
         # Define the url of the local file in the local directory
-        shared_input_file_url = 'file://%s/%s' % (os.getcwd(), 'input.txt')
-        staged_file = "%s%s" % (MY_STAGING_AREA, 'input.txt')
+        shared_input_file_url = 'file://%s/%s' % (os.getcwd(), 'trajectories.npy')
+        #shared_input_file_url = 'file://%s/%s' % (os.getcwd(), atom_file)
+        staged_file = "%s%s" % (MY_STAGING_AREA, 'trajectories.npy')
 
          # Configure the staging directive for to insert the shared file into
         # the pilot staging directory.
@@ -131,52 +103,49 @@ if __name__ == "__main__":
 
         # Configure the staging directive for shared input file.
         sd_shared = {'source': staged_file, 
-                     'target': 'input.txt',
+                     'target': 'trajectories.npy',
                      'action': rp.LINK
         }
 
         # Combine the ComputePilot, the ComputeUnits and a scheduler via
         # a UnitManager object.
-        print "Initializing Unit Manager ..."
+        #print "Initializing Unit Manager ..."
         umgr = rp.UnitManager (session=session,
                                scheduler=rp.SCHED_DIRECT)
 
-        umgr.register_callback(unit_state_cb)
+        #umgr.register_callback(unit_state_cb)
         # Add the created ComputePilot to the UnitManager.
-        print "Registering Compute Pilot with Unit Manager ..."
+        #print "Registering Compute Pilot with Unit Manager ..."
         umgr.add_pilots(pilot)
 
-        NUMBER_OF_TRAJECTORIES = total_file_lines
-         # i should define a "good" window based on division rules. in respect to the NUMBER_OF_TRAJECTORIES
+        NUMBER_OF_ATOMS = total_file_lines
+        WINDOW_SIZE =  win_size # i should define a "good" window based on division rules. in respect to the NUMBER_OF_ATOMS
         
 
         # submit CUs to pilot job
         cudesc_list = []
         sd_inter_in_list = list()
-        #print 'window is ' + str(WINDOW_SIZE)
-        #print 'traj count is ' + str(NUMBER_OF_TRAJECTORIES)
-        for i in range(1,NUMBER_OF_TRAJECTORIES+1,WINDOW_SIZE):
-            for j in range(i,NUMBER_OF_TRAJECTORIES,WINDOW_SIZE):
+        for i in range(1,NUMBER_OF_ATOMS+1,WINDOW_SIZE):
+            for j in range(i,NUMBER_OF_ATOMS,WINDOW_SIZE):
                     # I save here the results of each cu (matrix of all pairs) and stage it to create the graph
                 INTERMEDIATE_FILE = "distances_%d_%d.npz.npy" % (i-1,j-1)
                 sd_inter_out = {
                 'source': INTERMEDIATE_FILE,
                 # Note the triple slash, because of URL peculiarities
                 'target': 'staging:///%s' % INTERMEDIATE_FILE,
-                'action': rp.COPY
+                'action': rp.LINK
                 }
                 # -------- BEGIN USER DEFINED CU DESCRIPTION --------- #
                 cudesc = rp.ComputeUnitDescription()
                 cudesc.name='Euclidean_dist_%d_%d'%(i,j)
-                cudesc.pre_exec=['module load python/2.7.10']
+                cudesc.cores = 1
+                cudesc.pre_exec=['module load python']
                 cudesc.executable  = "python"
-                cudesc.cores = 4
-                cudesc.arguments   = ['atom_distances.py', WINDOW_SIZE, i, j, total_file_lines,cutoff] # each CU should compute window size distances 
+                cudesc.arguments   = ['atom_distances.py','trajectories.npy', WINDOW_SIZE, i, j, total_file_lines,cutoff] # each CU should compute window size distances 
                                                                         # I use i to calculate from which element I start calculating distances in each cu
                 cudesc.input_staging = ['atom_distances.py',sd_shared]
                 cudesc.output_staging = [sd_inter_out]
                 # -------- END USER DEFINED CU DESCRIPTION --------- #
-                #print 'i, j : ' + str(i) + str(j)
                 cudesc_list.append(cudesc)
 
                 # Configure the staging directive for input intermediate data - I will use it later
@@ -189,12 +158,12 @@ if __name__ == "__main__":
                 sd_inter_in_list.append(sd_inter_in)
 
         # Submit the previously created ComputeUnit descriptions to the
-        print "Submit Compute Units to Unit Manager ..."
+        #print "Submit Compute Units to Unit Manager ..."
         cu_set = umgr.submit_units (cudesc_list)
         cu_full_list.extend(cu_set)
-        print "Waiting for CUs to complete ..."
+        #print "Waiting for CUs to complete ..."
         umgr.wait_units()
-        print "All CUs completed successfully!"
+        #print "All CUs completed successfully!"
     
         # Create and submit a task to the pilot. This CU is going to aggregate the results of  the previous tasks
         # create a big graph and find all the connected components of the graph
@@ -203,39 +172,43 @@ if __name__ == "__main__":
         cudesc = rp.ComputeUnitDescription()
         cudesc.name='ConnComp'
         cudesc.executable = "python"
-        cudesc.pre_exec=['module load python/2.7.10','source /home/solejar/radical.pilot.sandbox/ve_comet/bin/activate']
-        #cudesc.pre_exec = ['module load python/'] # futuregrid machine 
-        cudesc.arguments = ['find_connencted_components.py',NUMBER_OF_TRAJECTORIES,WINDOW_SIZE]
+        cudesc.cores = 1
+        cudesc.pre_exec=['module load python']
+        cudesc.arguments = ['find_connencted_components.py',NUMBER_OF_ATOMS,WINDOW_SIZE]
         cudesc.input_staging = sd_inter_in_list
         cudesc.output_staging = [{'source':'components.npz.npy',
                                   'target':'components.npz.npy',
                                   'action':rp.TRANSFER}]
         # -------- END USER DEFINED CU DESCRIPTION --------- #
-        print "Submit subgraph task"
+        #print "Submit subgraph task"
         cuset = umgr.submit_units(cudesc)
         umgr.wait_units()
-        print "Task completed successfully!"
+        #print "Task completed successfully!"
 
         cu_full_list.append(cuset)
-        #How the output should be
 
-        ## Input topology/trajectory: md_prod_12x12_lastframe.pdb md_prod_12x12_everymicroS_pbcmolcenter.xtc
-        ## Head group atom selection: name P*
-        ## Number of lipids:          39024
-        ## Number of phosphates (P):  44784
-        ## [time(ps)] [N_leaflets] [N_lipids for the top 5 leaflets]
-        #1000000.0   2 21456 17568
-        #2000000.0   3 21455 17568     1
-        #3000000.0   6 21449 17567     4     2     1
-        #4000000.0  12 21448 17534    30     2     2
-        #5000000.0  10 21451 17562     2     3     1
-        #6000000.0   4 21452 17567     4     1
-        #7000000.0  15 21450 17458    87    15     3
-        #8000000.0  12 21451 17561     2     2     1
-        #9000000.0  18 21446 17439   114     6     3
-        #10000000.0  11 21441 17396   168     6     5
-        #11000000.0   9 21454 17563     1     1     1
+        #print "Creating Profile"
+        ProfFile = open('{0}.csv'.format(session_name),'w')
+        ProfFile.write('CU,Name,StageIn,Allocate,Exec,StageOut,Done\n')
+        for cu in cu_full_list:
+            timing_str=[cu.uid,cu.name,'N/A','N/A','N/A','N/A','N/A','N/A']
+            for states in cu.state_history:
+                if states.as_dict()['state']=='AgentStagingInput':
+                    timing_str[3]= (states.as_dict()['timestamp']-pilot.start_time).__str__()
+                elif states.as_dict()['state']=='Allocating':
+                    timing_str[4]= (states.as_dict()['timestamp']-pilot.start_time).__str__()
+                elif states.as_dict()['state']=='Executing':
+                    timing_str[5]= (states.as_dict()['timestamp']-pilot.start_time).__str__()
+                elif states.as_dict()['state']=='AgentStagingOutput':
+                    timing_str[6]= (states.as_dict()['timestamp']-pilot.start_time).__str__()
+                elif states.as_dict()['state']=='Done':
+                    timing_str[7]= (states.as_dict()['timestamp']-pilot.start_time).__str__()
 
+            ProfFile.write(timing_str[0]+','+timing_str[1]+','+
+                           timing_str[2]+','+timing_str[3]+','+
+                           timing_str[4]+','+timing_str[5]+','+
+                           timing_str[6]+','+timing_str[7]+'\n')
+        ProfFile.close()
 
 
     except Exception as e:
@@ -253,8 +226,8 @@ if __name__ == "__main__":
     finally:
         # always clean up the session, no matter if we caught an exception or
         # not.
-        print "Closing session"
-        session.close ()
+        #print "Closing session"
+        session.close (cleanup=False)
 
         # the above is equivalent to
         #

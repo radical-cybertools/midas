db3418ccc0850dddf7976f19e53ee391a69e3983
Georgios Chantzialexiou <georgeha98@gmail.com>
2017-05-23 19:00:02 -0500

    Add script using 2 producers

---
+++ streaming/kafka_producer-consumer-perfomance/multiple_producers/consumer.py
@@ -0,0 +1,38 @@
+from pykafka import KafkaClient
+import sys
+import time
+import dateutil
+import dateutil.parser
+
+
+broker = sys.argv[1]
+
+print broker
+
+
+client = KafkaClient(hosts=broker)
+print client.topics
+
+topic = client.topics['KmeansList']
+consumer = topic.get_simple_consumer()
+
+for message in consumer:
+    if message is not None:
+        print message.offset, message.value
+    else:
+        print 'None'
+
+
+while True:
+    message = consumer.consume(block=True)
+    #print message.value
+    now = time.time()
+    #sent_time=datetime.datetime.strptime(message.value, "%Y-%m-%dT%H:%M:%S.%fZ")
+    sent_time_string = message.value.split(";")[0]
+    sleep_time =float(message.value.split(";")[1])
+    sent_time = dateutil.parser.parse(sent_time_string)
+    sent_time_ts = time.mktime(sent_time.timetuple())
+    lat = now-sent_time_ts
+    result = "kafka, latency, 0, %.5f, %.5f, %s, %s\n"%(1/sleep_time, lat, message.value.split(";")[0], 
+                                                                                datetime.datetime.now().isoformat())
+    print(result)
---
+++ streaming/kafka_producer-consumer-perfomance/multiple_producers/producer.py
@@ -0,0 +1,42 @@
+from pykafka import KafkaClient
+from pykafka.partitioners import  hashing_partitioner
+from time import time
+import sys
+
+
+
+'''
+Producer is creating messages of equall size (100)
+
+
+'''
+
+if __name__ == "__main__":
+
+
+    broker = sys.argv[1]
+    number_of_points = int(sys.argv[2])
+    broker2 = sys.argv[3]
+    brokers = str(broker)  + ',' + broker2
+    client = KafkaClient(hosts=broker)
+    topic = client.topics['KmeansList']
+
+    ttc = time()
+    count = 0
+    size = 5
+    
+    n_of_messages = number_of_points / size
+    elements = list()
+
+    for j in xrange(size):
+        elements.append(j)
+    
+    el_str = str(elements)
+
+
+    with topic.get_sync_producer(partitioner=hashing_partitioner) as producer:
+        for i in xrange(n_of_messages):
+            producer.produce(el_str, partition_key='{}'.format(i))
+    ttc = time() - ttc
+
+    print ttc

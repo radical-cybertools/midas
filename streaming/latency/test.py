a61a0c708c3717f22bdd28f385e0a30104d39237
Georgios Chantzialexiou <georgeha98@gmail.com>
2017-08-05 13:14:37 -0500

    change msg size

--- streaming/latency/test.py
+++ streaming/latency/test.py
@@ -6,16 +6,19 @@ def utf8len(s):
     return len(s.encode('utf-8'))
 
 
-#msg_s = int(sys.argv[1])
+msg_s = int(sys.argv[1])
 
-msg_s = 5000
+#msg_s = 5000
 
 
 sleep_time = 1
 extra_size = []
 for i in xrange(msg_s):
     extra_size.append(msg_s)
-    
+
+
+
+
 extra_message_str = str(extra_size)
 
 print utf8len(extra_message_str)

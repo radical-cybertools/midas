f460e1aeef516ed75f7c82525062f240824248c7
Georgios Chantzialexiou <georgeha98@gmail.com>
2017-10-02 19:02:27 -0500

    Change exp setup

---
+++ streaming/StreamingKMeans/multiple_pilots/StreamingKMeans.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import time
+import datetime
+start = time.time()
+import logging
+logging.basicConfig(level=logging.WARN)
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.clustering import StreamingKMeans
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from pyspark  import  SparkContext, SparkConf
+import numpy as np
+import urllib2
+
+
+#######################################################################################
+# CONFIGURATIONS
+METABROKER_LIST= sys.argv[1]
+TOPIC= sys.argv[2]
+NUMBER_PARTITIONS = int(sys.argv[3])
+STREAMING_WINDOW=60
+#######################################################################################
+
+run_timestamp=datetime.datetime.now()
+RESULT_FILE= "results/kafka-streaming-" + run_timestamp.strftime("%Y%m%d-%H%M%S") + ".csv"
+
+try:
+    os.makedirs("results")
+except:
+    pass
+
+output_file=open(RESULT_FILE, "w")
+
+start = time.time()
+
+#output_file.write("Measurement,Number_Partitions, Time\n")
+#output_file.write("Spark Startup, %s, %.5f\n"%(NUMBER_PARTITIONS, time.time()-start))
+#output_file.flush()
+#######################################################################################
+
+decayFactor=1.0
+timeUnit="batches"
+model = StreamingKMeans(k=10, decayFactor=decayFactor, timeUnit=timeUnit).setRandomCenters(3, 1.0, 0)
+
+#def printOffsetRanges(rdd):
+#    for o in offsetRanges:
+#        print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
+
+def count_records(rdd):
+    print str(type(rdd))
+    if rdd!=None:
+        return rdd.collect()
+    
+    return [0]
+
+## OK
+def pre_process(datetime, rdd):
+    start = time.time()
+    points=rdd.map(lambda p: p[1]).flatMap(lambda a: eval(a)).map(lambda a: Vectors.dense(a))
+    end_preproc=time.time()
+    count = points.count()
+    #output_file.write("KMeans PreProcess, %d, %s, %.5f\n"%(count, NUMBER_PARTITIONS, end_preproc-start))
+    #output_file.flush()
+    return points
+
+## OK
+def model_update(rdd):
+    count = rdd.count()
+    start = time.time()
+    lastest_model = model.latestModel()
+    lastest_model.update(rdd, decayFactor, timeUnit)
+    end_train = time.time()
+    #output_file.write("KMeans Model Update, %d, %s, %.5f\n"%(count,NUMBER_PARTITIONS, end_train-start))
+    output_file.flush()
+
+appName="PythonSparkStreamingKafkaKMeans"
+conf = SparkConf().setAppName(appName).set('spark.metrics.conf.*.sink.csv.class','org.apache.spark.metrics.sink.CsvSink').set('spark.metrics.conf.*.sink.csv.directory','./')
+sc = SparkContext(conf=conf)
+
+ssc = StreamingContext(sc, STREAMING_WINDOW)
+kafka_dstream = KafkaUtils.createDirectStream(ssc, [TOPIC], {"metadata.broker.list": METABROKER_LIST })
+#output_file.write("Spark SSC Startup, %d, %s\n"%( NUMBER_PARTITIONS, str(ssc_end-ssc_start)))
+
+
+kafka_dstream.count().pprint()
+
+points = kafka_dstream.transform(pre_process)
+points.pprint()
+points.foreachRDD(model_update)
+
+
+try:
+    ssc.start()
+    ssc.awaitTermination()
+    ssc.stop(stopSparkContext=True, stopGraceFully=True)
+
+    output_file.close()
+
+finally:
+    ssc.stop(stopSparkContext=True, stopGraceFully=True)
+    sys.stdout.flush()
---
+++ streaming/StreamingKMeans/multiple_pilots/data_producer.py
@@ -0,0 +1,100 @@
+from pykafka import KafkaClient
+import numpy as np
+import os, sys
+import time
+import datetime
+sys.path.append("..")
+from pykafka.partitioners import hashing_partitioner
+
+NUMBER_CLUSTER=[100, 100, 100, 100, 100]
+NUMBER_POINTS_PER_CLUSTER=[100, 1000, 10000, 100000, 1000000]
+
+#NUMBER_CLUSTER=[100]
+#NUMBER_POINTS_PER_CLUSTER=[1000]
+NUMBER_DIM=3 # 1 Point == ~62 Bytes
+NUMBER_POINTS_PER_MESSAGE=[5000] # 3-D Point == 304 KB
+#NUMBER_POINTS_PER_MESSAGE=[10000] # 3-D Point == 304 KB
+INTERVAL=120
+NUMBER_OF_PRODUCES=1 # 10*60 = 10 minutes
+NUMBER_PARTITIONS=48
+TOPIC_NAME="Throughput"
+
+
+broker= sys.argv[1]
+
+
+client = KafkaClient(hosts=broker)
+topic = client.topics[TOPIC_NAME]
+producer = topic.get_sync_producer(partitioner=hashing_partitioner)
+
+run_timestamp=datetime.datetime.now()
+RESULT_FILE= "kafka-throughput-producer-" + run_timestamp.strftime("%Y%m%d-%H%M%S") + ".csv"
+STDOUT_FILE = "stdout-" + run_timestamp.strftime("%Y%m%d-%H%M%S") + ".csv"
+#STDOUT_LOOP_FILE = "stdout_loop-" + run_timestamp.strftime("%Y%m%d-%H%M%S") + ".csv"
+
+
+output_file=open(RESULT_FILE, "w")
+output_file.write("Number_Clusters,Number_Points_per_Cluster,Number_Dim,Number_Points_per_Message,Interval,Number_Partitions, Time\n")
+
+stdout_file = open(STDOUT_FILE, 'w')
+stdout_file.write('Produce_batch,Num_Messages,Number_APoints/Msg,KB_Transfered,KB/sec\n')
+
+#stdout_loop_file = open(STDOUT_LOOP_FILE,'w')
+#stdout_loop_file.write("Number,x_points,Time_to_produce_x_points\n")
+
+
+def get_random_cluster_points(number_points, number_dim):
+    mu = np.random.randn()
+    sigma = np.random.randn()
+    p = sigma * np.random.randn(number_points, number_dim) + mu
+    return p
+
+bytes = 0
+num_messages = 0
+
+count = 0
+global_start = time.time()
+for num_points_per_message in NUMBER_POINTS_PER_MESSAGE:
+    for idx, num_cluster in enumerate(NUMBER_CLUSTER):
+        count_produces = 0
+        num_point_per_cluster = NUMBER_POINTS_PER_CLUSTER[idx]
+        while count_produces < NUMBER_OF_PRODUCES:
+            start = time.time()
+            points = []
+            for i in range(num_cluster):
+                p = get_random_cluster_points(num_point_per_cluster, NUMBER_DIM)
+                points.append(p)
+            points_np=np.concatenate(points)
+            
+            number_batches = points_np.shape[0]/num_points_per_message
+            #print "Points Array Shape: %s, Number Batches: %.1f"%(points_np.shape, number_batches)
+            last_index=0
+            for i in range(number_batches):
+                stdout_file.write( "%d,%d,%d,%d,%.1f,%s\n"%\
+                                    (last_index,                                                                                           
+                                     last_index+num_points_per_message, 
+                                     num_messages,
+                                     num_points_per_message,
+                                     bytes/1024,
+                                     bytes/1024/(time.time()-global_start)))
+                points_batch = points_np[last_index:last_index+num_points_per_message]
+                points_strlist=str(points_batch.tolist())
+                producer.produce(points_strlist, partition_key='{}'.format(count))
+                count = count + 1
+                last_index = last_index + num_points_per_message
+                bytes = bytes + len(points_strlist)
+                num_messages = num_messages + 1
+                #time.sleep(INTERVAL)
+            
+            end = time.time()
+            #stdout_loop_file.write("%d,%d,%.1f\n"%(count_produces,end-start,num_cluster*num_point_per_cluster))
+            output_file.write("%d,%d,%d,%d,%d,%d,%.5f\n"%(num_cluster,num_point_per_cluster,NUMBER_DIM, 
+                                                       num_points_per_message,INTERVAL,NUMBER_PARTITIONS,(end-start)))
+            output_file.flush()
+            stdout_file.flush()
+            #stdout_loop_file.flush()
+            count_produces = count_produces + 1
+    
+        #time.sleep(INTERVAL)
+
+output_file.close()

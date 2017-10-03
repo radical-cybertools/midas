6c7df2c0667bee4024c9bad55f12fd64386bdbef
Georgios Chantzialexiou <georgeha98@gmail.com>
2017-10-03 15:30:08 -0500

    update script details

--- streaming/StreamingKMeans/multiple_pilots/submit_bot.py
+++ streaming/StreamingKMeans/multiple_pilots/submit_bot.py
@@ -4,58 +4,34 @@ __license__   = "MIT"
 
 import sys, os
 import radical.pilot as rp
+import radical.utils as ru
 import numpy as np
 
 #os.environ['RADICAL_PILOT_PROFILER']= 'TRUE'
 os.environ['RADICAL_PILOT_VERBOSE']= 'DEBUG'
 
-#------------------------------------------------------------------------------
-def pilot_state_cb (pilot, state):
-
-    if not pilot:
-        return
-
-    print "[Callback]: ComputePilot '%s' state: %s." % (pilot.uid, state)
-
-    if state == rp.FAILED:
-        sys.exit (1)
-#------------------------------------------------------------------------------
-#
-def unit_state_cb (unit, state):
-
-    if not unit:
-        return
-    global CNT
-
-    print "[Callback]: unit %s on %s: %s." % (unit.uid, unit.pilot, state)
-
-    if state == rp.FAILED:
-        print "stderr: %s" % unit.stderr
-        sys.exit(2)
-#------------------------------------------------------------------------------
 #
 if __name__ == "__main__":
 
     session = rp.Session()
     print "session id: %s" % session.uid
+    broker_string = sys.argv[1] + ':9092'
 
-    c = rp.Context('ssh')
-    c.user_id = "tg829618"
-    session.add_context(c)
     try:
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
         print "Initializing Pilot Manager ..."
         pmgr = rp.PilotManager(session=session)
-        pmgr.register_callback(pilot_state_cb)
+        #pmgr.register_callback(pilot_state_cb)
 
-        pdesc = rp.ComputePilotDescription ()
+        pdesc = rp.ComputePilotDescription()
         pdesc.resource = "xsede.wrangler"  # this is a "label", not a hostname
-        pdesc.cores    = 48
-        pdesc.runtime  = 15  # minutes
+        pdesc.cores    = 24
+        pdesc.runtime  = 20  # minutes
         pdesc.cleanup  = False  # clean pilot sandbox and database entries
         pdesc.project = 'TG-MCB090174'
-        #pdesc.project = 'TG-MCB090174:dssd+TG-MCB090174+2396'
+        pdesc.project = 'TG-MCB090174:dssd+TG-MCB090174+2397'
+        pdesc.access_schema = 'gsissh'
 
         # submit the pilot.
         print "Submitting Compute Pilot to Pilot Manager ..."
@@ -64,7 +40,7 @@ if __name__ == "__main__":
         # create a UnitManager which schedules ComputeUnits over pilots.
         print "Initializing Unit Manager ..."
         umgr = rp.UnitManager (session=session)
-        umgr.register_callback(unit_state_cb)
+        #umgr.register_callback(unit_state_cb)
 
         # Add the created ComputePilot to the UnitManager.
         print "Registering Compute Pilot with Unit Manager ..."
@@ -72,7 +48,7 @@ if __name__ == "__main__":
 
         print "Creating a session"
         ## ------ EXPERIMENTAL CONFIGURATIONS------------------------------------------#
-        NUMBER_OF_PRODUCERS = 3
+        NUMBER_OF_PRODUCERS = 1
        #--------------------------------------------------------------------------------
 
         cudesc_list =[]
@@ -87,7 +63,7 @@ if __name__ == "__main__":
             #--------END USER DEFINED CU DESCRIPTION----------------------------#
 
         print "Submit Compute Units to Unit Manager ..."
-        cu_set = umgr.submit_units (cudesc_list)
+        cu_set = umgr.submit_units(cudesc_list)
 
         print "Waiting for CUs to complete ..."
         umgr.wait_units()
@@ -96,10 +72,11 @@ if __name__ == "__main__":
 
     except Exception as e:
         print "caught Exception: %s" % e
+        ru.print_exception_trace()
         raise
     except (KeyboardInterrupt, SystemExit) as e:
         print "need to exit now: %s" % e
-
+        ru.print_exception_trace()
     finally:
         print "closing session"
         session.close ()

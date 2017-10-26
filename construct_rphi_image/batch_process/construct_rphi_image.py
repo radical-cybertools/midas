6d85778e369f13f2f1664bd08600f61a59d4d0b0
George Chantzialexiou <georgeha98@gmail.com>
2017-10-25 20:16:09 -0400

    checkpoint

--- construct_rphi_image/batch_process/construct_rphi_image.py
+++ construct_rphi_image/batch_process/construct_rphi_image.py
@@ -7,89 +7,24 @@ import os
 import datetime
 import time
 
-
-run_timestamp=datetime.datetime.now()
-RESULT_FILE= "results/batch-construct_phi-" + run_timestamp.strftime("%Y%m%d-%H%M%S") + ".csv"
-
-try:
-    os.makedirs('results')
-except:
-    pass
-
-output_file = open(RESULT_FILE, "a")
-output_file.write("Image_Dimensions,Image_size(MB),Iteration,TTC")
-
-start = time.time()
-
-## Generate the image
-# first generate some random scattering pattern
-# There are missing regions
-
-pixel_sizes = [400,800,1600,3200]
-pixel_sizes = [400]
-for pixels in pixel_sizes:
-
-    shape = pixel_sizes, pixel_sizes
-    x0,y0 = 401, 401
-    ANGLES = angle_grid((y0, x0), shape)
-    RADII = radial_grid((y0, x0), shape)
-    img = np.cos(ANGLES*5)**2*RADII**2
-
-    mask = np.ones_like((ANGLES))
-    mask[100:200] = 0
-    mask[:,100:200] = 0
-    mask[:,500:643] = 0
-    mask[70:130] = 0
-
-    img*=mask
-
-    print (img.nbytes/1024)/1024  , 'MB'
-
-    # reconstruct the image into polar grid
-    ttr = time.time()
-    rphibinstat = RPhiBinnedStatistic(shape, bins=(400,360), mask=mask, origin=(y0,x0))
-    rphi_img = rphibinstat(img)
-    # create mask from np.nans since RPhiBinnedStatistic fills masked regions with np.nans
-    rphimask = np.ones_like(rphi_img)
-    rphimask[np.where(np.isnan(rphi_img))] = 0
-    ttrpolar = time.time() - ttr
-
-    ## Regenerate the image to thes the construct_rphi_image
-
-    # get angles and radii from (q, phi) polar coordinate system
-    angles = rphibinstat.bin_centers[1]
-    radii = rphibinstat.bin_centers[0]
-
-    # reproject
-    Zproj = construct_rphi_avg_image(radii, angles, rphi_img, shape=(800,800))
-
-
-    # 10  fold symmetry
-    # Let's add the same image but before reconstructing, shift phi by 2pi/10....
-
-    sym = int(10)
-    polar_shape = 500, 360 
-    origin = x0, y0
-
 def reconstruct_nfold(img, sym, polar_shape, mask=None, origin=None):
-        ''' 
-        Reconstruct an image assuming a certain symmetry.
+    ''' 
+    Reconstruct an image assuming a certain symmetry.
+        
+    Parameters
+    ----------
+    img : the image
             
-        Parameters
-        ----------
-        img : the image
-                
-        sym : the symmetry of the sample
-                
-        polar_shape : the shape of the new polar coordinate image
+    sym : the symmetry of the sample
             
-        Returns
-        -------
-        reconstructed_image : the reconstructed  image
-            
-        '''
+    polar_shape : the shape of the new polar coordinate image
+        
+    Returns
+    -------
+    reconstructed_image : the reconstructed  image   
+    '''
+    shape = 0
     shape = img.shape
-
     rphibinstat = RPhiBinnedStatistic(shape, bins=polar_shape, mask=mask, origin=origin)
     angles = rphibinstat.bin_centers[1]
     radii = rphibinstat.bin_centers[0]
@@ -110,13 +45,101 @@ def reconstruct_nfold(img, sym, polar_shape, mask=None, origin=None):
         reconstructed_image[w] += imgtmp[w]
         reconstructed_image_mask += (~np.isnan(imgtmp)).astype(int)
             
-        # the mask keeps count of included pixels. Average by this amount
-    reconstructed_image /= reconstructed_image_mask
+    # the mask keeps count of included pixels. Average by this amount
+    try:
+        reconstructed_image /= reconstructed_image_mask
+    except Exception as e:
+        print  'Division failed at line 50' , reconstructed_image_mask
+        pass 
     
     return reconstructed_image
 
+
+
+
+run_timestamp=datetime.datetime.now()
+RESULT_FILE= "results/batch-construct_phi-" + run_timestamp.strftime("%Y%m%d-%H%M%S") + ".csv"
+
+try:
+    os.makedirs('results')
+except:
+    pass
+
+output_file = open(RESULT_FILE, "a")
+output_file.write("Image_Dimensions,Image_size(MB),Iteration,TTC\n")
+
+
+## Generate the image
+# first generate some random scattering pattern
+# There are missing regions
+
+dims = 400  # 1 MB
+dims = 600   #2 MB
+dims = 800   #4MB
+dims = 1050 # 8MB
+dims = 1450 # 16 MB
+dims = 2050 #32 MB
+dims = 2900  # 64 MB
+dims = 4100   # 128 MB
+dims = 5800   # 256 MB
+
+
+pixel_sizes = [400,600,800,1050,1450,2050,2900,4100,5800]
+# 1MB, 2MB, 4MB, 8MB, 16MB, 32MB, 64MB, 128MB, 256MB
+for pixels in pixel_sizes:
+
+    shape = pixels, pixels
+    x0,y0 = 401, 401
+    ANGLES = angle_grid((y0, x0), shape)
+    RADII = radial_grid((y0, x0), shape)
+    img = np.cos(ANGLES*5)**2*RADII**2
+
+    mask = np.ones_like((ANGLES))
+    mask[100:200] = 0
+    mask[:,100:200] = 0
+    mask[:,500:643] = 0
+    mask[70:130] = 0
+
+    img*=mask
+    image_size = (img.nbytes/1024)/1024
+
+    #print image_size  , 'MB'
+
+    start = time.time()
+
+    # reconstruct the image into polar grid
+    ttr = time.time()
+    rphibinstat = RPhiBinnedStatistic(shape, bins=(400,360), mask=mask, origin=(y0,x0))
+    rphi_img = rphibinstat(img)
+    # create mask from np.nans since RPhiBinnedStatistic fills masked regions with np.nans
+    rphimask = np.ones_like(rphi_img)
+    rphimask[np.where(np.isnan(rphi_img))] = 0
+    ttrpolar = time.time() - ttr
+
+    ## Regenerate the image to thes the construct_rphi_image
+
+    # get angles and radii from (q, phi) polar coordinate system
+    angles = rphibinstat.bin_centers[1]
+    radii = rphibinstat.bin_centers[0]
+
+    # reproject
+    Zproj = construct_rphi_avg_image(radii, angles, rphi_img, shape=(pixels,pixels))
+
+
+    # 10  fold symmetry
+    # Let's add the same image but before reconstructing, shift phi by 2pi/10....
+
+    sym = int(10)
+    polar_shape = 500, 360 
+    origin = x0, y0
+
+
     reconstructed_image = reconstruct_nfold(img, sym, polar_shape, mask=mask, origin=origin)
 
-    end = time.time() - start
+    ttc = time.time() - start
+    "Image_Dimensions,Image_size(MB),Iteration,TTC"
+    output_file.write('%d,%d,%d,%f\n'%(pixels,image_size,1,ttc))
 
-    print 'Total time is: ', end , ' seconds
+    #print 'Total time is: ', ttc , ' seconds'   
+
+

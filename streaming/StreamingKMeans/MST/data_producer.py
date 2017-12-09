5bf3693e89b8e028ef464f274caa4ddb1ab46c94
George Chantzialexiou <georgeha98@gmail.com>
2017-12-09 17:31:15 -0500

    checkpoint

--- streaming/StreamingKMeans/MST/data_producer.py
+++ streaming/StreamingKMeans/MST/data_producer.py
@@ -3,7 +3,7 @@
 """
 Created on Sat Dec  9 15:05:49 2017
 
-@author: Georgos Chantzialexiou
+@author: Georgios Chantzialexiou
 """
 
 
@@ -18,7 +18,6 @@ from pykafka.partitioners import hashing_partitioner
 
 def setup_kafka_producer(broker):
 
-    global producer
     
     client = KafkaClient(hosts=broker)
     topic = client.topics[TOPIC_NAME]
@@ -36,7 +35,7 @@ def setup_metrics():
     stdout_file.write('Produce_batch_from,Produce_batch_until,\
         Num_Messages,Number_of_points_per_Message,\
         Number_Dim,Message_size_in_Bytes,\
-        APoints/Msg,KB_Transfered,KB/sec,TimeStamp\n')
+        APoints/Msg,KB_Transfered,KB/sec,TimeStamp,ConcurentProducers\n')
     
     return
 
@@ -74,18 +73,19 @@ def generate_messages_and_save_them_to_np_array(Number_of_messages,\
     
 
 
-def publish_messages(msg_number, msg_np):
+def publish_messages(msg_number, msg_np, producer):
     
     run_timestamp=datetime.datetime.now()
     ts = run_timestamp.strftime("%Y%m%d-%H%M%S")
     last_index = msg_number*MESSAGE_OF_POINTS_PER_MESSAGE
-    points_batch = points_np[last_index:last_index+MESSAGE_OF_POINTS_PER_MESSAGE]
+    points_batch = msg_np[last_index:last_index+MESSAGE_OF_POINTS_PER_MESSAGE]
     points_strlist=str(points_batch.tolist())
     message_size_in_bytes =  len(points_strlist)/1024  # in bytes
-    tbytes = bytes + len(points_strlist)
+    global tbytes
+    tbytes = tbytes + len(points_strlist)
     producer.produce(points_strlist, partition_key='{}'.format(msg_number))
     
-    stdout_file.write("%d,%d,%d,%d,%d,%d, %.1f,%s,%s\n"%\
+    stdout_file.write("%d,%d,%d,%d,%d,%d, %.1f,%s,%s,%s\n"%\
                                     (last_index,                                                                                           
                                      last_index+MESSAGE_OF_POINTS_PER_MESSAGE, 
                                      msg_number,
@@ -94,40 +94,39 @@ def publish_messages(msg_number, msg_np):
                                      message_size_in_bytes,
                                      tbytes/1024,
                                      tbytes/1024/(time.time()-global_start),
-                                     ts))
+                                     ts,concurrent_producers))
     stdout_file.flush()
 
     return
-            
-                
-                #tbytes = bytes + len(points_strlist)
-            
-            #end = time.time()
-            
 
-
-
-    
 
 if __name__ == '__main__':
     
-    NUMBER_OF_MESSAGES =  10000
+    NUMBER_OF_MESSAGES =  6000  # duration expectation: 4 minutes
     NUMBER_OF_DIM = 3
     MESSAGE_OF_POINTS_PER_MESSAGE = 5000
     
     #broker = sys.argv[1]
-    #producer = setup_kafka_producer(broker)
-    #setup_metrics()
+    #concurrent_producers = sys.argv[2]
+    broker = 'c251:9092'
+    concurrent_producers  = 4
+    
+    
+    
+    producer = setup_kafka_producer(broker)
+    setup_metrics()
+    global tbytes
+    
     tbytes = 0
     
     
     global_start = time.time()
-    #msg_list = generate_messages_and_save_them_to_np_array(NUMBER_OF_MESSAGES,\
-    #                             MESSAGE_OF_POINTS_PER_MESSAGE, NUMBER_OF_DIM)
+    msg_list = generate_messages_and_save_them_to_np_array(NUMBER_OF_MESSAGES,\
+                                 MESSAGE_OF_POINTS_PER_MESSAGE, NUMBER_OF_DIM)
     
     
-    #for i in xrange(NUMBER_OF_MESSAGES):
-    #    publish_messages(msg_number=i,msg_np=msg_list)
+    for i in xrange(NUMBER_OF_MESSAGES):
+        publish_messages(i,msg_list,producer)
         
         
         

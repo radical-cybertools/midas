1b8cdde6e4d1291fdbcc6d9f1c6a26eddf25c54b
chengwill97 <chengwill97@gmail.com>
2018-04-24 16:25:50 -0400

    added arguments

--- watershed/Chocolate/watershed.py
+++ watershed/Chocolate/watershed.py
@@ -26,26 +26,51 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # pilot cores
-    parser.add_argument('cores',                type=int,
+    parser.add_argument('cores',
+                        type=int,
                         help='pilot cores: specify the number of cores to use')
     # num compute units
-    parser.add_argument('cu',                   type=int,
+    parser.add_argument('cu',
+                        type=int,
                         help='compute units: specify the number of CUs to use')
-    # pilot cores
-    parser.add_argument('images',               type=int,
+    # number images
+    parser.add_argument('images',
+                        type=int,
                         help='specify the number of images to analyze')
-    # brightness
-    parser.add_argument('-b','--brightness',    type=int, default=0, choices=[0, 1],
-                        help='set image background brightness')
-    # report name
-    parser.add_argument('-r','--report',        type=str, default='watershed_report',
-                        help='report name')
+    # project
+    parser.add_argument('project',
+                        type=str,
+                        help='specify the project to use')
     # resource
-    parser.add_argument('-R','--resource',      type=str, default='local.localhost_anaconda',
+    parser.add_argument('resource',
+                        type=str,
                         help='specify the resource to use')
+    # walltime
+    parser.add_argument('-w', '--walltime',
+                        type=int,
+                        default=15,
+                        help='specify the walltime in minutes (defaults to 15)')
+    # brightness
+    parser.add_argument('-b', '--brightness',
+                        type=int,
+                        default=0, 
+                        choices=[0, 1],
+                        help='set image background brightness (defaults to 0)')
+    # report name
+    parser.add_argument('-r', '--report',
+                        type=str,       
+                        default='watershed_report',
+                        help='report name (defaults to "watershed_report")')
+    # data path to input and output folders
+    parser.add_argument('-p', '--path',
+                        type=str,
+                        default='./',
+                        help='path of data input and output folders (defaults to cwd)')
     # verbosity
-    parser.add_argument('-v', '--verbosity',    action='count', default=0,
-                        help='increase output verbosity')
+    parser.add_argument('-v', '--verbosity',
+                        action='count', 
+                        default=0,
+                        help='increase output verbosity (defaults to 0)')
 
     # retrieve arguments
     args = parser.parse_args()
@@ -54,20 +79,30 @@ if __name__ == '__main__':
     pilot_cores         = args.cores
     number_of_CUs       = args.cu
     number_of_images    = args.images
-    report_name         = args.report
+    project             = args.project
     resource            = args.resource
-    bright_background   = args.brightness          
+    walltime            = args.walltime
+    bright_background   = args.brightness
+    report              = args.report
+    path                = os.path.abspath(args.path)
+    verbosity           = args.verbosity
 
-    if args.verbosity >= 2:
+    if 'local' in resource:
+        project = None
+
+    if verbosity >= 2:
         print('Input Arguments')
-        pp([   ['pilot_cores      ' , pilot_cores],
-               ['number_of_CUs    ' , number_of_CUs],
-               ['number_of_images ' , number_of_images],
-               ['report_name      ' , report_name],
-               ['resource         ' , resource],
-               ['bright_background' , bright_background]
+        pp([   ['pilot_cores      ' , pilot_cores       ],
+               ['number_of_CUs    ' , number_of_CUs     ],
+               ['number_of_images ' , number_of_images  ],
+               ['project          ' , project           ],
+               ['resource         ' , resource          ],
+               ['walltime         ' , walltime          ],
+               ['bright_background' , bright_background ],
+               ['report           ' , report            ],
+               ['path             ' , path              ],
            ])
-    if args.verbosity >= 1:
+    if verbosity >= 1:
         print 'Arguments are valid'
 
     # Create a new session. No need to try/except this: if session creation fails, there is not much we can do anyways...
@@ -87,20 +122,17 @@ if __name__ == '__main__':
         #
         pdesc = rp.ComputePilotDescription()
         pdesc.resource  = resource
-        #pdesc.project = 'TG-MCB090174'      # for Stampede/Wrangler
-        # pdesc.project   = 'unc100'             # for Comet/Gordon
-        pdesc.runtime   = 12                   # minutes
+        pdesc.project   = project
+        pdesc.runtime   = walltime
         pdesc.cores     = pilot_cores
 
         pilot = pmgr.submit_pilots(pdesc)
-
 
 
         # Combine the ComputePilot, the ComputeUnits and a scheduler via a UnitManager object.
         umgr = rp.UnitManager(session=session)
 
         umgr.add_pilots(pilot) 
-        
         
         images_in_each_CU = number_of_images / number_of_CUs
         additional_load = number_of_images % number_of_CUs
@@ -120,10 +152,18 @@ if __name__ == '__main__':
             cudesc.executable  = 'python'
             
             if (additional_load == 0):
-                cudesc.arguments = ['watershed_lines.py', step, step+images_in_each_CU-1, bright_background]
+                cudesc.arguments = ['watershed_lines.py', 
+                                    path, 
+                                    step, 
+                                    step+images_in_each_CU-1, 
+                                    bright_background]
                 step += images_in_each_CU
             else:
-                cudesc.arguments = ['watershed_lines.py', step, step+images_in_each_CU  , bright_background]
+                cudesc.arguments = ['watershed_lines.py', 
+                                    path,
+                                    step, 
+                                    step+images_in_each_CU, 
+                                    bright_background]
                 step += images_in_each_CU + 1
                 additional_load -= 1
 
